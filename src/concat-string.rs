907b7880f5fc57dc50d77473aeab0a1fa83a98c7
jerry <profkiti@gmail.com>
2024-03-19 13:45:25 +0100

    complete string concatenation on ownerhship, borrowing & references

--- src/concat-string.rs
+++ src/concat-string.rs
@@ -2,8 +2,7 @@ fn main() {
     let mut string1 = String::from("Hello");
     let mut string2 = String::from(" World");
 
- concatenate_strings(&mut string1, &mut string2);
-
+    concatenate_strings(&mut string1, &mut string2);
 }
 
 fn concatenate_strings (string1: &mut String,string2: &mut String) {
